f4de31763cf9dccf03120438a1359fbdcbfcb81b
Elias Levy <fearsome.lucidity@gmail.com>
2018-05-22 16:57:05 -0700

    Enforce UTF-8 encoding in the direct mapping library
    Previously we used the JNA OPTION_STRING_ENCODING to force UTF-8 encoding when JNA transcodes Java Strings into C/C++ char arrays/pointers, rather than depend on the default system encoding.  When I implemented the direct mapping for the `hs_scan` method, it no longer enforced the encoding for that method.

--- src/main/java/com/gliwka/hyperscan/jna/HyperscanLibraryDirect.java
+++ src/main/java/com/gliwka/hyperscan/jna/HyperscanLibraryDirect.java
@@ -2,11 +2,16 @@ package com.gliwka.hyperscan.jna;
 
 import com.sun.jna.*;
 
+import java.util.HashMap;
+
 public class HyperscanLibraryDirect {
 
     public static native int hs_scan(Pointer database, String data, int length, int flags, Pointer scratch, HyperscanLibrary.match_event_handler callback, Pointer context);
 
     static {
-        Native.register("hs");
+        HashMap<String,Object> opts = new HashMap<>();
+        opts.put(Library.OPTION_STRING_ENCODING, "UTF-8");
+
+        Native.register(NativeLibrary.getInstance("hs", opts));
     }
 }
