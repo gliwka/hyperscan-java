2c7048eae0c06d5959eddb71e478f710f0690571
Matthias Gliwka <mg@cerebuild.com>
2018-06-08 09:10:06 +0200

    Reencrypt pgp keys

--- travis-build/deploy-maven-central.sh
+++ travis-build/deploy-maven-central.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo Decrypting pgp key for release signing
-openssl aes-256-cbc -K $encrypted_1acfec761134_key -iv $encrypted_1acfec761134_iv -in travis-build/codesigning.enc -out /tmp/codesigning.asc
-
+openssl aes-256-cbc -K $encrypted_b3fbb85b63b3_key -iv $encrypted_b3fbb85b63b3_iv -in travis-build/codesigning.enc -out /tmp/codesigning.asc -d
 echo Importing pgp key into gpg
 gpg --fast-import /tmp/codesigning.asc
 
